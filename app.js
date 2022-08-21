062fe7d7708d1593bbbcfc1bfe3971edacef3db6
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2022-08-21 01:15:58 -0500

    Completed Exercise

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+const firstName = "Kelly";
+let lastName;
+var age;
+
+lastName = "Allen";
+age = 53;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(selectedSize);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
+
+
+
+
+
